130f413d30a69a8152e26ee01fa91d6c302f80e6
SharlynneYap <Sharlynneyap@gmail.com>
2025-11-28 10:21:27 +0800

    Initial commit

---
+++ src/main/kotlin/com/TelemondoActivity1/TelemondoActivity1/TelemondoActivity1Application.kt
@@ -0,0 +1,11 @@
+package com.TelemondoActivity1.TelemondoActivity1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TelemondoActivity1Application
+
+fun main(args: Array<String>) {
+	runApplication<TelemondoActivity1Application>(*args)
+}
---
+++ src/test/kotlin/com/TelemondoActivity1/TelemondoActivity1/TelemondoActivity1ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.TelemondoActivity1.TelemondoActivity1
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TelemondoActivity1ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
